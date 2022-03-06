8257b0fa0277c3ddc3b58b6145bd1c6c64bdb385
JoyfulPSD <101010432+JoyfulPSD@users.noreply.github.com>
2022-03-06 10:36:29 +0900

    Update coinPOWRWithAI.py

--- coinPOWRWithAI.py
+++ coinPOWRWithAI.py
@@ -70,7 +70,7 @@ while True:
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-POWR", 0.1)
             current_price = get_current_price("KRW-POWR")
-            if target_price < current_price:
+            if target_price < current_price and current_price < predicted_close_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order("KRW-POWR", krw*0.9995)
