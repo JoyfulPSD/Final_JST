e7fa148e939804303a116b3e4282bb2df2625473
JoyfulPSD <101010432+JoyfulPSD@users.noreply.github.com>
2022-04-09 19:07:23 +0900

    Update and rename CoinJSTwitAI.py to CoinJSTwithAI.py

--- CoinJSTwithAI.py
+++ CoinJSTwithAI.py
@@ -70,7 +70,7 @@ while True:
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-JST", 0.1)
             current_price = get_current_price("KRW-JST")
-            if target_price < current_price < target_price * 1.005 and current_price < predicted_close_price:
+            if target_price < current_price < target_price * 1.005 and current_price < predicted_close_price * 1.03:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order("KRW-JST", krw*0.9995)
